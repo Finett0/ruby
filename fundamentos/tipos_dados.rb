6b5f96802dbda2ad5d304b815a02a58670c9b429
Finett0 <finettogs@gmail.com>
2025-06-25 07:37:38 -0300

    data types

---
+++ fundamentos/tipos_dados.rb
@@ -0,0 +1,21 @@
+puts "Olá".class
+
+puts 1.class
+
+puts 10.5.class
+
+range = 1..10
+
+puts range.class
+
+range.each do |item|
+    puts item
+end
+
+puts range.include?15
+
+dicionario = {name: "Giovanni", "age" => 32}
+
+array = [1,2,3,4,5]
+
+puts array.class
