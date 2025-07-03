9e52745bc247ec6a2b7feb10624752044e72875c
Finett0 <finettogs@gmail.com>
2025-07-03 18:55:56 -0300

    exe01

--- poo/exercicio01.rb
+++ poo/exercicio01.rb
@@ -2,3 +2,35 @@ class Cachorro
     attr_accessor :nome
     attr_reader :raça
 
+    def initialize(nome,raça='Golden')
+        @nome = nome
+        @raça = raça
+    end
+
+    def latir(latido="Au au")
+        puts latido
+    end
+end
+
+cachorro1 = Cachorro.new("Apollo")
+puts cachorro1.nome
+puts cachorro1.raça
+
+class Papagaio
+    attr_accessor :nome
+    attr_accessor :idade
+
+    def initialize(nome,idade)
+        @nome = nome
+        @idade = idade
+    end
+
+    def repitir_frase(frase="Ué")
+        puts frase
+    end
+end
+
+papagaio1 = Papagaio.new("Chico",2)
+puts papagaio1.nome
+puts papagaio1.idade
+puts papagaio1.repitir_frase
