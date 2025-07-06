641dc1d6d4d9afbc26183779f86b84710377c245
Finett0 <finettogs@gmail.com>
2025-07-06 12:15:27 -0300

    classe_constantes

---
+++ poo/constantes_classes_constantes.rb
@@ -0,0 +1,9 @@
+URL = "https://ruby-doc.org/" # Uma constante se delcara com as letras maisculas e são feitas para os valores não serem alterados
+
+class ClassesConstantes
+  NOME_APP = "Sistema de CRM"
+  NOME_CLIENTE = "CRM X"
+end
+
+puts ClassesConstantes::NOME_APP
+puts ClassesConstantes::NOME_CLIENTE
